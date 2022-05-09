8028f505718d4a7356b3cbdd36d9deb21d366446
taclearning22 <taclearning22@gmail.com>
2022-05-09 18:05:32 +0530

    feat: Add code for module 6

---
+++ OdeToFood1/Pages/Restaurants/ClientRestaurants.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OdeToFood1.Pages.Restaurants
+{
+    public class ClientRestaurantsModel : PageModel
+    {
+
+    }
+}
